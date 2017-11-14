4c6f59692a52dbb6de37dfb594ff5c9466cf45d6
Elvin_1492 <elvin.arzumanoglu@gmail.com>
2017-11-14 17:17:54 +0300

    Web services

---
+++ FileAchiverService/FileAchiverServiceInstaller.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel;
+using System.Configuration.Install;
+using System.ServiceProcess;
+
+namespace FileAchiverService
+{
+	[RunInstaller(true)]
+	public class FileAchiverServiceInstaller : Installer
+	{
+		public FileAchiverServiceInstaller()
+		{
+			var serviceInstaller = new ServiceInstaller();
+			serviceInstaller.StartType = ServiceStartMode.Manual;
+			serviceInstaller.ServiceName = FileAchiver.SericeName;
+			serviceInstaller.DisplayName = FileAchiver.SericeName;
+
+			Installers.Add(serviceInstaller);
+
+			var processInstaller = new ServiceProcessInstaller();
+			processInstaller.Account = ServiceAccount.User;
+			processInstaller.Username = @".\Elvin";
+			processInstaller.Password = "12345";
+
+			Installers.Add(processInstaller);
+		}
+	}
+}
---
+++ FileAchiverService/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+
+namespace FileAchiverService
+{
+	static class Program
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		static void Main()
+		{
+			string currentDir = Path.GetDirectoryName(Path.GetFullPath(Process.GetCurrentProcess().MainModule.FileName));
+
+			string inQueue = @".\private$\todoQueue";
+			string outQueue = @".\private$\doneQueue"; ;
+
+			//Debugger.Launch();
+			// InstallUtil.exe "D:\mentoring\window servises\Samples\FileProcessorService\bin\Debug\FileProcessorService.exe"
+
+			var fileProcessor = new FileAchiver(inQueue, outQueue, 1, 10);
+
+			ServiceBase.Run(fileProcessor);
+		}
+	}
+}
